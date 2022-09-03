0fe35dfd4501ed591867232f9a439cfef049d366
Sean <andricoseanwork@gmail.com>
2022-09-03 23:05:17 +0800

    Navigating through pages

--- public/pages/index.tsx
+++ public/pages/index.tsx
@@ -1,9 +1,18 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
     <div>
       <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+      </ul>
     </div>
   );
 };
