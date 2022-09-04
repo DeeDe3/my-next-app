b2c7331c7da2c7943f144ac8b3eb97c19a0833d4
Sean <andricoseanwork@gmail.com>
2022-09-04 21:03:36 +0800

    different way of setting dynamic links

--- public/pages/clients/index.tsx
+++ public/pages/clients/index.tsx
@@ -1,9 +1,31 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Clients: NextPage = () => {
+  const clients = [
+    { id: "sean", name: "Andrico Sean" },
+    { id: "miller", name: "River Miller" },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((clients) => (
+          <li key={clients.id}>
+            <Link
+              href={{
+                pathname: "/clients/[id]",
+                query: {
+                  id: clients.id,
+                },
+              }}
+            >
+              {clients.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
