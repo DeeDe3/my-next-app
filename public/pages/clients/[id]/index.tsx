5682e84af8d38b71e07aa91d1da4eeffa0bffa71
Sean <andricoseanwork@gmail.com>
2022-09-04 21:12:29 +0800

    navigating programatically

--- public/pages/clients/[id]/index.tsx
+++ public/pages/clients/[id]/index.tsx
@@ -1,9 +1,21 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
 
 const ClientsProject: NextPage = () => {
+  const router = useRouter();
+
+  function loadProjectHandler() {
+    // *load data..
+    router.push({
+      pathname: "/clients/[id]/[clientId]",
+      query: { id: "sean", clientId: "projectb" },
+    });
+  }
+
   return (
     <div>
-      <h1>The Porjects of a given client Page</h1>
+      <h1>The Porjects of a {router.query.id} Page</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
